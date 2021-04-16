b8ccc1fbc95ec0b9260d2658f49bc9e1733f304b
seokrae <kslbsh@gmail.com>
2021-04-16 23:15:20 +0900

    docs: http 요청, 응답 코드

---
+++ spring-servlet/src/main/java/com/example/basic/request/RequestParamServlet.java
@@ -0,0 +1,53 @@
+package com.example.basic.request;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+/**
+ * 1. 파라미터 전송 기능
+ * http://localhost:8080/request-param?username=hello&age=20
+ *
+ */
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        System.out.println("[전체 파라미터 조회] - start");
+
+        Enumeration<String> parameterNames = request.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            String element = parameterNames.nextElement();
+            System.out.println("element = " + element);
+        }
+//        request.getParameterNames().hasIterator()
+//                .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
+
+        System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
+
+        System.out.println("[단일 파라미터 조회]");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+        System.out.println();
+
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        String[] usernames = request.getParameterValues("username");
+        if(usernames != null) {
+            for (String name : usernames) {
+                System.out.println("username = " + name);
+            }
+        }
+
+        response.getWriter().write("ok");
+    }
+}
